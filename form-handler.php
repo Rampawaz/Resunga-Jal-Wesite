7f8595a6f690ad62170eeea1aadacefec913b140
Ram Panthi <rampawaz@gmail.com>
2024-08-14 20:04:19 +0545

    modified the title

--- form-handler.php
+++ form-handler.php
@@ -5,7 +5,6 @@ $Visitor_email = $_get['email'];
 $subject = $_post['subject'];
 $message = $_post['message'];
 
-$email_from= 'info@resungajal.com.np';
 
 $email_subject= 'New Form Submission';
 
@@ -13,7 +12,7 @@ $email_body ="User Name: $name. \n".
 "User Email: $Visitor_email. \n".
 "Subject: $subject. \n".
 "User Message: $message. \n";
-$to= 'jalresunga@gmail.com' ;
+$to= 'info@resungajal.com.np' ;
  $header ="From: $email_from \r\n";
 
  $header .= "Reply-To: $visitor_email \r\n";
