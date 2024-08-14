41ba3b6f80d0cb6a791df384f364f990bf0503a5
Ram Panthi <rampawaz@gmail.com>
2024-08-14 16:00:53 +0545

    modified the title

--- form-handler.php
+++ form-handler.php
@@ -13,7 +13,7 @@ $email_body ="User Name: $name. \n".
 "User Email: $Visitor_email. \n".
 "Subject: $subject. \n".
 "User Message: $message. \n";
-
+$to= "jalresunga@gmail.com" ;
  $header ="From: $email_from \r\n";
 
  $header .= "Reply-To: $visitor_email \r\n";
