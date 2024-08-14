2b61c80ff0c11cc7bff488b2149a833c867e63e1
Ram Panthi <rampawaz@gmail.com>
2024-08-14 22:29:01 +0545

    modified the title

---
+++ sendmail.php
@@ -0,0 +1,67 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if(isset($_POST['submitcontact']))
+{
+    $fullname = $_POST['full_name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'jalresunga@gmail.com';                     //SMTP username
+    $mail->Password   = 'ckogeknyeibyagol';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('jalresunga@gmail.com', 'Mailer');
+    $mail->addAddress('info@resungajal.com', 'Joe User');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+   // $mail->addReplyTo('info@example.com', 'Information');
+   // $mail->addCC('cc@example.com');
+   // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+   // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+   // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'New Enquiry- Resunga Jal Contact Form';
+    $mail->Body    = '<h3> Hello you got a new Enquiry </h3>
+    <h4> Fullname: '.$fullname.'</h4>
+    <h4> Email: '.$email.'</h4>
+    <h4> Subject: '.$subject.'</h4>
+    <h4> Message: '.$message.'</h4>
+    ';
+   // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+}
+else{
+    header('Location: contact.html');
+    exit(0);
+}
+
+
+
+
+?>
