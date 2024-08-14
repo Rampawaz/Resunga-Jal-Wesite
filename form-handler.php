d63c150d24ab8c6746f68b4d38ab0f7b21f5ce72
Ram Panthi <rampawaz@gmail.com>
2024-08-14 19:47:25 +0545

    modified the title

--- form-handler.php
+++ form-handler.php
@@ -1,11 +1,11 @@
 <?php
-if{(isset($_post['submit']))
+
 $name = $_post['name'];
 $Visitor_email = $_get['email'];
 $subject = $_post['subject'];
 $message = $_post['message'];
 
-$email_from= "info@resungajal.com.np";
+$email_from= 'info@resungajal.com.np';
 
 $email_subject= 'New Form Submission';
 
@@ -13,7 +13,7 @@ $email_body ="User Name: $name. \n".
 "User Email: $Visitor_email. \n".
 "Subject: $subject. \n".
 "User Message: $message. \n";
-$to= "jalresunga@gmail.com" ;
+$to= 'jalresunga@gmail.com' ;
  $header ="From: $email_from \r\n";
 
  $header .= "Reply-To: $visitor_email \r\n";
@@ -21,7 +21,7 @@ $to= "jalresunga@gmail.com" ;
  mail($to,$email_subject,$email_body,$headers);
 
  header ("Location: contact.html")
-}
+
 
 
 
